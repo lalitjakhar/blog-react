69d72aab8fbe9c6ff5c79eb9cf221d385bcdf5d9
Lalit Jakhar <lalitjakhar@Rajeshs-iMac.local>
2023-11-20 18:59:26 +0530

    initial commit

--- server/controller/user-controller.js
+++ server/controller/user-controller.js
@@ -1,15 +1,24 @@
 import user from "../model/user.js";
-const LiveController = {}
+import bcrypt from "bcrypt";
+
+const LiveController = {};
 
 LiveController.signupUser = async (request, response) => {
   try {
-    const userData = request.body;
-    console.log(userData, "899999999998999");
+    const hashedPassword = await bcrypt.hash(request.body.password, 10);
 
-    const newUser = new user(userData);
+    const userdata = {
+      username: request.body.username,
+      name: request.body.name,
+      password: hashedPassword,
+    };
+
+    const newUser = new user(userdata);
     await newUser.save();
 
-    return response.status(200).json({ msg: "Signup successful" });
+    return response
+      .status(200)
+      .json({ response: response, msg: "Signup successful" });
   } catch (error) {
     return response
       .status(500)
